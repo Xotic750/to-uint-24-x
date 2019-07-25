575d70e80f7a18af431c20de97765761d80778b1
Graham Fairweather <xotic750@gmail.com>
2019-07-26 00:24:51 +0200

    :bookmark: v4.0.13
    Auto build

--- dist/to-uint-24-x.js
+++ dist/to-uint-24-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-24T16:47:48.633Z",
+  "date": "2019-07-25T22:24:45.188Z",
   "describe": "",
   "description": "Converts a value to Uint24.",
   "file": "to-uint-24-x.js",
   "hash": "2afe7b91014e92f54211",
   "license": "MIT",
-  "version": "4.0.12"
+  "version": "4.0.13"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
